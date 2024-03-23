ad4c3dc2d63c683f087ca84b67b16090d886b145
code-looney <danielvbofficieel@gmail.com>
2024-03-23 20:53:14 +0100

    created header components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Container from "./components/Container";
 
 export default function App() {
   return (
-    <Container className="bg-main h-screen">
+    <Container className="bg-main h-screen grid place-content-center">
       <Header />
     </Container>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import Container from './Container';
 import Title from './Title';
 import Sub from './Sub';
+import Logo from './Logo';
 
 const Header = () => {
   return (
-    <Container className={`bg-header flex`}>
-        <Title>My balance</Title>
-        <Sub>$921.48</Sub>
+    <Container className={`bg-header flex items-center justify-center w-96 h-24 rounded-xl`}>
+        <Container className={`w-[340px] flex justify-between items-center text-white `}>
+          <Container>
+            <Title className={`font-light`}>My balance</Title>
+            <Sub className={`text-[24px] font-medium`}>$921.48</Sub>
+          </Container>
+          <Container>
+            <Logo />
+          </Container>
+        </Container>
+
     </Container>
   )
 }
