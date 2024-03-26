419f4f17fde9407fe0dcf4fedfea4d269bcd1979
code-looney <danielvbofficieel@gmail.com>
2024-03-26 14:43:17 +0100

    created graphs

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -7,8 +7,6 @@ import Total from './Total';
 const Chart = () => {
     const [days, setDays] = useState();
     const [amount, setAmount] = useState();
-    const [mon, setMon] = useState();
-    const [monAmount, setMonAmount] = useState();
     const height = ['h-[17.45%]', 'h-[34.91%]', 'h-[52.36%]', 'h-[31.07%]', 'h-[23.39%]', 'h-[43.28%]', 'h-[25.48%]', 'h-[23.39%]'];
 
     useEffect(() => {
@@ -16,8 +14,9 @@ const Chart = () => {
         .then(res => res.json())
         .then(data => {
             setDays(data)
-            // setMon(data[0].day)
-            // setMonAmount(JSON.stringify(data[0].amount))
+            data.map(amount => {
+                setAmount(amount.amount)
+            })
         })
     }, []) 
 
@@ -28,22 +27,21 @@ const Chart = () => {
         <Container className={`w-[340px] h-[340px] flex flex-col gap-3`}>
             <ChartTitle className={`text-[26px] text-chart-font-color tracking-wide font-medium`}>Spending - Last 7 days</ChartTitle>
             <Container className={`h-[280px] gap-5 flex flex-col justify-end`}>                
-                <Container className={`outline flex items-end justify-center h-[100%]`}>
-                    <ul className={`flex justify-end items-center text-[12px] h-[100%] font-thin`}>
+                <Container className={`flex items-end justify-center h-[100%]`}>
+                    <ul className={`flex justify-end items-end gap-4 text-[12px] h-[100%] font-thin`}>
                          {/* {days && days.map(amount => <li>{console.log(amount.amount)}</li>)} */}
                         
                          {days && days.map((day, index) => {
                             return (
-                                <li key={index}>
-                                    <div>
-                                        <div className='flex flex-col items-center'>{monAmount}</div>
-                                        <div className={`bg-black w-8 h-[${monAmount}%] rounded-sm`}></div>
+                                <li className={`text-[10px] h-[100%]`} key={index}>
+                                    <div className={`flex flex-col h-[100%] items-center justify-end`}>
+                                        <div className={``}>{day.amount}</div> {/* make event */}
+                                        <div className={`bg-black w-8 h-[${day.amount}%] rounded-sm`}></div>
                                         <div>{day.day}</div>
                                     </div>
                                 </li>
                             )
-                         })}
-
+                        })}
                     </ul>
                 </Container>
                 <Container>
